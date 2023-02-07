604fcb4a5962cf1c68f42fa63dbced34e546d0b4
Audrey-Randall <aurandal@ucsd.edu>
2023-02-07 15:44:28 -0800

    Better error handling

--- analyze.py
+++ analyze.py
@@ -4,6 +4,7 @@ import requests
 import json
 import time
 from datetime import datetime
+import argparse
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
@@ -18,6 +19,20 @@ def bearer_oauth(r):
     r.headers["User-Agent"] = "v2RetweetedByPython"
     return r
 
+def make_request(url, all_params):
+    # Make request
+    response = requests.request("GET", url, auth=bearer_oauth, params=all_params)
+
+    # Handle "too many requests" error
+    if response.status_code == 429:
+        print("{}: Request returned an error (too many requests?) {}".format(datetime.now(), response.text))
+        return None
+
+    # Handle all other errors
+    if response.status_code != 200:
+        print("Request returned an error: {} {}".format(response.status_code, response.text))
+        return None
+    return response
 
 def connect_to_endpoint(url, params):
     responses = []
@@ -29,23 +44,17 @@ def connect_to_endpoint(url, params):
         else:
             all_params = '&'.join(params)
 
-        # Make request
-        response = requests.request("GET", url, auth=bearer_oauth, params=all_params)
-
-        # Handle "too many requests" error
-        if response.status_code == 429:
-            print("{}: Request returned an error (too many requests?) {} Sleeping and continuing.".format(datetime.now(), response.text))
+        
+        resp = make_request(url, all_params)
+        if resp is None:
+            print("Sleep 10 and retry request once...")
             time.sleep(10)
-            continue
-
-        # Handle all other errors
-        if response.status_code != 200:
-            raise Exception(
-                "Request returned an error: {} {}".format(
-                    response.status_code, response.text
-                )
-            )
-        resp_json = response.json()
+            resp = make_request(url, all_params)
+            if resp is None:
+                print("Request failed twice, moving on.")
+                break
+            
+        resp_json = resp.json()
         if 'error' in resp_json:
             raise Exception(
                 "Error in response: {}".format(resp_json['error'])
@@ -60,14 +69,17 @@ def connect_to_endpoint(url, params):
         responses.append(resp_json)
     return responses
 
-def parse(input_file, output_file):
+def get_conversation(conversation_id):
+    url = 'https://api.twitter.com/2/tweets/search/recent?query=conversation_id:1279940000004973111'
+
+def get_tweets(input_file, output_file):
     user_fields = "user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,protected,public_metrics,url,username,verified,withheld"
     tweet_fields = "tweet.fields=attachments,author_id,context_annotations,conversation_id,created_at,edit_controls,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,reply_settings,source,text,withheld"
     outfile = open(output_file, 'w')
     with open(input_file) as f:
         reader = csv.DictReader(f, delimiter='\t')
         for i, row in enumerate(reader):
-            if i < 14314:
+            if i < 17800:
                 continue
             time.sleep(3)
             # retweet_url = "https://api.twitter.com/2/tweets/{}/retweeted_by".format(row['tweetId'])
@@ -80,6 +92,40 @@ def parse(input_file, output_file):
                 outfile.write(str(t)+'\n')
     outfile.close()
 
-parse('notes-00000.tsv', 'test.json')
-# ids = ['1377030478167937024','1536848327979016193']
-# print(','.join(ids))
+def get_retweets(input_file, output_file):
+    tweet_fields = "tweet.fields=attachments,author_id,context_annotations,conversation_id,created_at,edit_controls,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,reply_settings,source,text,withheld"
+    outfile = open(output_file, 'w')
+    with open(input_file) as f:
+        reader = csv.DictReader(f, delimiter='\t')
+        for i, row in enumerate(reader):
+            # if i < 17127:
+            #     continue
+
+            # Get retweets.
+            # Only 75 requests allowed in 15 minutes.
+            # time.sleep(12)
+            retweet_url = "https://api.twitter.com/2/tweets/{}/retweeted_by".format(row['tweetId'])
+            retweet_resp = connect_to_endpoint(retweet_url, [tweet_fields])
+
+            # Get replies (the conversation).
+            converation_url = 'https://api.twitter.com/2/tweets/search/recent?query=conversation_id:{}'.format(row['tweetId'])
+            conversation_resp = connect_to_endpoint(retweet_url, [tweet_fields])
+
+            for t in retweet_resp:
+                # for key in row:
+                #     t['cn_'+key] = row[key]
+                outfile.write(str(t)+'\n')
+            break
+    outfile.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('function')
+    args = parser.parse_args()
+
+    if args.function == 'tweets':
+        get_tweets('notes-00000.tsv', 'test.json')
+    elif args.function == 'retweets':
+        get_retweets('notes-00000.tsv', 'retweet_test.json')
+    else:
+        print('Usage: specify either "tweets" or "retweets"')
